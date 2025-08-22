2e51040ee532ed8393f943969e72903c73937a35
asmanisar929 <asmanisar929@gmail.com>
2025-08-22 08:40:24 -0700

    connect db

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "./constants.js";
+const app = express();
+import connectDB from "./db/index.js";
+//require("dotenv").config({ path: "./.env" });    //either use required or below import ur choice
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+connectDB();
+// **** first methof of connecting database ****
+
+/*(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.error("Express error:", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();*/
